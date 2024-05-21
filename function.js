afb162414f822d611b03a019b09276f952fc8d9a
alkeshalku <120634387+alkeshalku@users.noreply.github.com>
2024-05-21 02:39:27 -0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+function.js
+function add(a,b){
+    let c=a+b;
+    console.log(c)
+}
+add(1,2);
+
+// function
+function fullname(fname,iname){
+    console.log(`${fname} ${iname}`)
+}
+fullname("aniyo","bugs")
---
+++ script.js
@@ -0,0 +1,73 @@
+// console.log("hello world")
+//  let a=10;
+//  let b=20;
+
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// prefix increment
+// let c=2;
+// let d=c++;
+// console.log(c);
+// console.log(d);
+
+// let person={
+//     fname :"tiya",
+//     age:10
+// }
+// console.log(person.fname);
+
+// let car={
+//     model:("celerio"),
+//     color:"white"
+// };
+
+// console.log(car);
+
+// let person={
+//     fname:"tiya reba stephen",
+//     address:{
+//         street:"abc",
+//         place:"xyx",
+//         pin:123
+//     },
+// age:10
+// }
+// console.log(persons.address.place)
+// var toothbresh=new object();
+// toothbresh.name="colgate";
+// toothbresh.color"white";
+// console.log(toothbresh);
+// concatination
+// let fname="tiya";
+// let iname="stephen";
+// console.log(`$(fname) $(iname)`)
+// let m=5;
+// let n=6;
+// console.log(`the sun is $(m+n)`)
+// array
+// fruits=["apple","mango","orange",23]
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits.length)
+
+// array
+// 
+
+// array of objects
+let bsc =[
+    {
+        fname: "anto",
+        age: 10,
+    }
+    {
+        fname: "christo",
+        age: 11,
+    }
+    fname: "elias",
+      age: 12,
+]
+console.log(bsc(0)=age);
